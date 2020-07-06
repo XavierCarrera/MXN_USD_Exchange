a416e90f87c082d712b5c3b898b4c519ab54b869
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-07-06 18:19:20 -0500

    Final Exchange Version

--- Money_Exchange.py
+++ Money_Exchange.py
@@ -1,3 +1,10 @@
+def exchange(pesos_type, value_usd):
+    usd = input("How many USD you want to exchange? ")
+    usd = float(usd)
+    pesos = usd / value_usd
+    pesos = round(pesos, 2)
+    pesos = str(pesos)
+    print("You currently have $" + pesos )
 menu = """
 Welcome to the Money Exchange 💲
 
@@ -5,38 +12,18 @@ Welcome to the Money Exchange 💲
 2. ARG
 3. MXN
 
-Choose one of the currencies in order to exchange to USD.
+Choose one of the currencies in order to exchange it to USD.
 
 """
-
-opcion = input(menu)
+ 
+opcion = input(menu) 
 
 if opcion == "1":
-    usd = input("How many USD you want to exchenge to COP? ")
-    usd = float(usd)
-    cop_value = 0.00027
-    cop = usd / cop_value
-    cop = round(cop, 2)
-    cop = str(cop)
-    print("You currently have $" + cop + " COP")
+    exchange("COP", 0.00027)
 elif opcion == "2":
-    pass
-    usd = input("How many USD you want to exchenge to ARG? ")
-    usd = float(usd)
-    arg_value = 0.014
-    arg = usd / arg_value
-    arg = round(arg, 2)
-    arg = str(arg)
-    print("You currently have $" + arg + " ARG")
+    exchange("ARG", 0.014)
 elif opcion == "3":
-    usd = input("How many USD you want to exchenge to MXN? ")
-    usd = float(usd)
-    mxn_value = 0.045
-    mxn = usd / mxn_value
-    mxn = round(mxn, 2)
-    mxn = str(mxn)
-    print("You currently have $" + mxn + " MXN")
+    exchange("MXN", 0.045)
 else:
     print("Choose a valid option")
-
 
